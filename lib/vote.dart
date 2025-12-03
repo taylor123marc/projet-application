a93836050de55e861fadfbdc670d18876a47243e
Mamefatim14 <Mamefatimdione@gmail.com>
2025-12-03 14:25:10 -0500

    Interface vote 2.0

---
+++ lib/vote.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class VotePage extends StatefulWidget {
+   VotePage({super.key});
+
+  @override
+  State<VotePage> createState() => _VotePageState();
+}
+
+class _VotePageState extends State<VotePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(title:  Text('Vote')),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
+}
